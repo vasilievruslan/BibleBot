9c9a51901ac52875cd77a8bc8083316f2bf86bad
Ruslan Vasiliev <ruzero.io@gmail.com>
2019-09-03 11:21:02 +0300

    first commit

--- index.js
+++ index.js
@@ -1,59 +1,67 @@
 const Telegraf = require('telegraf');
-const fetch = require('node-fetch');
-const request = require("request");
-const apiKey = process.env.apiKey;
-const botToken = process.env.botToken;
+const axios = require('axios');
+const botToken = process.env.BOT_TOKEN;
+const bot = new Telegraf(botToken)
+const url = 'https://api.bibleonline.ru/bible';
+const bible = require('./ru_synodal.json');
+const verses = require('./verses.json');
+const hashCode = (s) => {
+  return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);
+};
 
-
-function search(query = '', cb) {
-  var options = {
-    method: 'GET',
-    headers: { 'api-key': apiKey },
-    url: 'https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-02/search',
-    qs: { query: query }
-  };
-
-  request(options, function (error, response, body) {
-    if (error) throw new Error(error);
-    let res = JSON.parse(body)
-    cb(res)
+const search = async (query = 'Ин 3.16') => {
+  let res;
+  let data = await axios.get(url, {
+    params: {
+      callback: "bible",
+      trans: "rus",
+      max: "5",
+      q: query
+    }
   });
-}
+  data = JSON.parse(data.data.split('(')[1].split(')')[0]);
+  if (data.length !== 0) {
 
-// bot.on('inline_query', ({ inlineQuery, answerInlineQuery }) => {
-//   search(inlineQuery.query, (res) => {
-//     const results = res.data.verses.map((verse) => ({
-//       type: 'article',
-//       id: verse.id,
-//       title: verse.reference,
-//       input_message_content: verse.text
-//     }));
-//     answerInlineQuery(results);
-//   })
-// })
+    var title = data[0].h2
+    res = data.slice(1).map(item => {
+      return {
+        title,
+        body: item.v.t
+      }
+    })
+  }else {
+    res = verses;
+  }
+  return res;
+};
 
-async function omdbSearch (query = '') {
-  const apiUrl = `http://www.omdbapi.com/?s=${query}&apikey=9699cca`
-  const response = await fetch(apiUrl)
-  const json = await response.json()
-  const posters = (json.Search && json.Search) || []
-  return posters.filter(({ Poster }) => Poster && Poster.startsWith('https://')) || []
-}
 
-const bot = new Telegraf(botToken);
-
-bot.on('inline_query', async ({ inlineQuery, answerInlineQuery }) => {
-  console.log(inlineQuery);
-  const posters = await omdbSearch(inlineQuery.query)
-  const results = posters.map((poster) => ({
-    type: 'photo',
-    id: poster.imdbID,
-    caption: poster.Title,
-    description: poster.Title,
-    thumb_url: poster.Poster,
-    photo_url: poster.Poster
+bot.on('inline_query', async ({update, inlineQuery, answerInlineQuery }) => {
+  console.log(update.inline_query.from)
+  let results;
+  let versesRes = await search(inlineQuery.query)
+  // console.log(versesRes);
+  results = versesRes.map((verse, i) => ({
+    type: 'article',
+    id: hashCode(verse.title + i +  Math.random()),
+    title: verse.title,
+    description: verse.body,
+    input_message_content: {
+      message_text: `${verse.body} (${verse.title})`,
+    }
   }))
+
   return answerInlineQuery(results)
 })
 
+
+// bot.on('text', async (ctx) => {
+//   const query = ctx.message.text
+//   const verses = await search(query);
+//
+//   return ctx.reply(`${verses[1].v.t}, (${verses[0].h2})`)
+// })
+
+// console.log(search('бт 1 1'))
 bot.launch()
+console.log('bot started')
