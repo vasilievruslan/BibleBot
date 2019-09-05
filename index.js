cc8089a7e4c729d8233daad2806a547754326635
Ruslan Vasiliev <ruzero.io@gmail.com>
2019-09-05 11:53:53 +0300

    search by words

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bot = new Telegraf(botToken)
 const url = 'https://api.bibleonline.ru/bible';
 const bible = require('./ru_synodal.json');
 const verses = require('./verses.json');
+const reg = /(\d*)\s*([а-я]+)\s*(\d+)(?:.(\d+))?(\s*-\s*(\d+)(?:\s*([а-я]+)\s*(\d+))?(?::(\d+))?)?/i
 const hashCode = (s) => {
   return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);
 };
@@ -13,7 +14,28 @@ require('http').createServer().listen(process.env.PORT || 5000).on('request', fu
     res.end('')
 })
 
-const search = async (query = 'Ин 3.16') => {
+
+
+
+const search = (query) => {
+  var res = [];
+  var count = 10;
+  bible.forEach(el => {
+    el.chapters.forEach((chapter, chapterIndex) => {
+      chapter.forEach((verse, verseIndex) => {
+        if(verse.toLowerCase().includes(query.toLowerCase())) {
+          res.push({
+            title: `${el.abbrev} ${chapterIndex + 1}:${verseIndex + 1}`,
+            body: verse
+          })
+        }
+      })
+    })
+  });
+  return res.slice(0, 10);
+}
+
+const searchLink = async (query = 'Ин 3.16') => {
   let res;
   let data = await axios.get(url, {
     params: {
@@ -41,10 +63,15 @@ const search = async (query = 'Ин 3.16') => {
 
 
 bot.on('inline_query', async ({update, inlineQuery, answerInlineQuery }) => {
-  console.log(update.inline_query.from)
+  // console.log(update.inline_query.from)
   let results;
-  let versesRes = await search(inlineQuery.query)
-  // console.log(versesRes);
+  let versesRes;
+  if (reg.test(inlineQuery.query)) {
+    versesRes = await searchLink(inlineQuery.query);
+  } else {
+    versesRes = search(inlineQuery.query);
+  }
+  console.log(inlineQuery.from.username + ': ' + inlineQuery.query);
   results = versesRes.map((verse, i) => ({
     type: 'article',
     id: hashCode(verse.title + i +  Math.random()),
@@ -58,14 +85,6 @@ bot.on('inline_query', async ({update, inlineQuery, answerInlineQuery }) => {
   return answerInlineQuery(results)
 })
 
-
-// bot.on('text', async (ctx) => {
-//   const query = ctx.message.text
-//   const verses = await search(query);
-//
-//   return ctx.reply(`${verses[1].v.t}, (${verses[0].h2})`)
-// })
-
 // console.log(search('бт 1 1'))
 bot.launch()
-console.log('bot started')
+// console.log('bot started')
